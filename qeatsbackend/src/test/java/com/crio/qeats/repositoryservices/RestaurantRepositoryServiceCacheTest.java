9855f9b9ed2b019de51bc762858227e99e7b2763
Crio.GitLab <admin@example.com>
2019-06-06 05:32:00 +0000

    Update RestaurantRepositoryServiceCacheTest.java

--- qeatsbackend/src/test/java/com/crio/qeats/repositoryservices/RestaurantRepositoryServiceCacheTest.java
+++ qeatsbackend/src/test/java/com/crio/qeats/repositoryservices/RestaurantRepositoryServiceCacheTest.java
@@ -46,4 +46,29 @@ class RestaurantRepositoryServiceCacheTest {
     assertNotNull(restaurantRepositoryService);
 
     when(mockRestaurantRepository.findAll()).thenReturn(listOfRestaurants());
+     
+    Jedis jedis = GlobalConstants.getJedisPool().getResource();
+
+    // call it twice
+    List<Restaurant> allRestaurantsCloseBy = restaurantRepositoryService
+        .findAllRestaurantsCloseBy(20.0, 30.0, LocalTime.of(18, 1), 3.0);
+    allRestaurantsCloseBy = restaurantRepositoryService
+        .findAllRestaurantsCloseBy(20.0, 30.0, LocalTime.of(18, 1), 3.0);
+    GeoHash geoHash = GeoHash.withCharacterPrecision(20.0, 30.0, 7);
+
+    verify(mockRestaurantRepository, times(1)).findAll();
+    assertNotNull(jedis.get(geoHash.toBase32()));
+    assertEquals(2, allRestaurantsCloseBy.size());
+    assertEquals("11", allRestaurantsCloseBy.get(0).getRestaurantId());
+    assertEquals("12", allRestaurantsCloseBy.get(1).getRestaurantId());
+  }
+
+  private List<RestaurantEntity> listOfRestaurants() throws IOException {
+    String fixture =
+        FixtureHelpers.fixture(FIXTURES + "/initial_data_set_restaurants.json");
+
+    return objectMapper.readValue(fixture, new TypeReference<List<RestaurantEntity>>() {
+    });
+  }
+}
 
