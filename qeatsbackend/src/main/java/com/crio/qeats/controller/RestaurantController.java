56b0d0e15ca4461b8c58b4cff34c73a37037259d
Crio User <crio-user@adityatumarada.asia-south1-c.c.prod-workspaces.internal>
2019-06-07 19:46:24 +0530

    fixed spot bugs

--- qeatsbackend/src/main/java/com/crio/qeats/controller/RestaurantController.java
+++ qeatsbackend/src/main/java/com/crio/qeats/controller/RestaurantController.java
@@ -12,6 +12,9 @@ import com.crio.qeats.services.RestaurantService;
 
 import java.time.LocalTime;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,9 +22,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 
 @RestController
